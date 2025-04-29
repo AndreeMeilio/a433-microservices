e5e1607d3deb5dcdfa05d59d9fa6fb15d53a2502
AndreeMeilio <meilioandree27@gmail.com>
2025-04-29 09:54:05 +0700

    adding script to build and push docker image

---
+++ build_push_image_karsajobs_ui.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+
+registry=ghcr.io
+namespace=andreemeilio
+imagename=karsajobs-ui
+
+docker build -t ${registry}/${namespace}/${imagename}:latest .
+echo $CR_PAT | docker login ${registry} -u USERNAME --password-stdin
+docker push ${registry}/${namespace}/${imagename}:latest
